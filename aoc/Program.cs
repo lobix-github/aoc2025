8fc7dd967738f8000affd72e8e0fa420ef8901de
Łukasz Łobacz <lulo@demant.com>
2025-12-05 08:46:33 +0100

    Day 5/1

--- aoc/Program.cs
+++ aoc/Program.cs
@@ -1,4 +1,5 @@
 ﻿//new d01().Run();
 //new d02().Run();
 //new d03().Run();
-new d04().Run();
+//new d04().Run();
+new d05().Run();
---
+++ aoc/d05.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+
+class d05 : baseD
+{
+    public void Run()
+    {
+        List<(long, long)> fresh = new();
+        List<(long, long)> ranges = new();
+		HashSet<long> available = new();
+
+		var lines = File.ReadLines(@"..\..\..\inputs\05.txt").ToList();
+
+		bool isAvailable = false;
+		foreach (var line in lines)
+		{
+			if (line.Length == 0 && !isAvailable)
+			{
+				isAvailable = true;
+				continue;
+			}
+
+			if (!isAvailable)
+			{
+				fresh.Add((line.Split('-')[0].ToInt64(), line.Split('-')[1].ToInt64()));
+				continue;
+			}
+
+			available.Add(line.ToInt64());
+		}
+
+		fresh = fresh.OrderBy(x => x.Item1).ToList();
+		var start = fresh.First().Item1;
+		var end = fresh.First().Item2;
+		for (int i = 1; i < fresh.Count; i++)
+		{
+			var f = fresh[i];
+			if (f.Item1 > end)
+			{
+				ranges.Add((start, end));
+				start = f.Item1;
+				end = f.Item2;
+				continue;
+			}
+
+			if (f.Item2 > end)
+			{
+				end = f.Item2;
+			}
+		}
+		ranges.Add((start, end));
+
+		var count = available.Sum(a => ranges.Where(f => a >= f.Item1 && a <= f.Item2).Count());
+		Console.WriteLine(count);
+	}
+}
