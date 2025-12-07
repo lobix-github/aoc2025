db7f9855fce1f06dd99fa64957dc3963de9578dd
lobix <lobix@LOBIX-PC>
2025-12-07 15:35:38 +0100

    Day 7/2

--- aoc/d07.cs
+++ aoc/d07.cs
@@ -1,5 +1,4 @@
-﻿
-class d07
+﻿class d07
 {
 	public void Run()
 	{
@@ -7,6 +6,8 @@ class d07
 
 		var lines = File.ReadLines(@"..\..\..\inputs\07.txt").ToList();
 		beams.Add(lines[0].IndexOf('S'));
+		var cols = Enumerable.Repeat(0L, lines[0].Length).ToList();
+		cols[lines[0].IndexOf('S')]++;
 
 		var count = 0;
 		foreach (var line in lines.Skip(1))
@@ -16,14 +17,16 @@ class d07
 				if (line[i] == '^' && beams.Contains(i))
 				{
 					beams.Add(i - 1);
+					cols[i - 1] += cols[i];
 					beams.Add(i + 1);
+					cols[i + 1] += cols[i];
 					beams.Remove(i);
+					cols[i] = 0;
 					count++;
 				}
 			}
-
 		}
-
 		Console.WriteLine(count);
+		Console.WriteLine(cols.Sum());
 	}
 }
